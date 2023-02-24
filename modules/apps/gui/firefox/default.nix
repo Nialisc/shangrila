fcd2cf324585e672c6da3671e5b6a14c56c3df0a
Sylvain Colignon <sylvain.colignon@nialis.xyz>
2023-02-24 09:56:31 +0100

    feat(firefox): Add user settings

--- modules/apps/gui/firefox/default.nix
+++ modules/apps/gui/firefox/default.nix
@@ -10,9 +10,28 @@ in
 
   config = mkIf cfg.enable {
     environment = {
-      systemPackages = with pkgs; [ firefox ];
       sessionVariables = {
         XDG_DESKTOP_DIR = "$HOME";
+      };
+    };
+
+    shangrila.home.extraOptions = {
+      programs.firefox = {
+        enable = true;
+
+        profiles.${config.shangrila.user.name} = {
+          id = 0;
+          name = config.shangrila.user.name;
+          settings = {
+            "browser.aboutwelcome.enabled" = false;
+            "browser.meta_refresh_when_inactive.disabled" = true;
+            "browser.bookmarks.showMobileBookmarks" = false;
+            "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.aboutConfig.showWarning" = false;
+            "browser.ssb.enabled" = true;
+          };
+        };
       };
     };
   };
