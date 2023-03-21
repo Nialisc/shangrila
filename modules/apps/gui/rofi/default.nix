e2b71c8d12c7a0788af7da642db0307606391a03
Sylvain Colignon <sylvain.colignon@nialis.xyz>
2023-03-21 13:11:47 +0100

    feat(rofi): Add theme

--- modules/apps/gui/rofi/default.nix
+++ modules/apps/gui/rofi/default.nix
@@ -8,5 +8,13 @@ in
     environment = {
       systemPackages = with pkgs; [ rofi ];
     };
+
+    shangrila.home.extraOptions = hm: {
+      home.file.".config/rofi" = {
+        source =
+          hm.config.lib.file.mkOutOfStoreSymlink "/home/nialis/.nix/modules/apps/gui/rofi/config";
+        recursive = true;
+      };
+    };
   };
 }
