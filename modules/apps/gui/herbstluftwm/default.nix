6acca1aee398e36e2bb5e511b8dab6c4d8e812b6
Sylvain Colignon <sylvain.colignon@nialis.xyz>
2023-03-21 22:00:47 +0100

    feat(herbsluftwm): Switching

--- modules/apps/gui/herbstluftwm/default.nix
+++ modules/apps/gui/herbstluftwm/default.nix
@@ -1,12 +1,11 @@
-{ options, config, lib, pkgs, ... }:
+{ options, config, pkgs, lib, inputs, ... }:
 
 with lib;
-let 
-  cfg = config.shangrila.apps.gui;
+let cfg = config.shangrila.apps.gui;
 in
 {
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ bspwm ];
+    environment.systemPackages = with pkgs; [ herbstluftwm ];
     services = {
       xserver = {
         enable = true;
@@ -16,31 +15,30 @@ in
         xkbOptions = "eurosign:e";
         libinput = enabled;
         displayManager.startx = enabled;
-        displayManager.defaultSession = "none+bspwm";
-        windowManager.bspwm = enabled;
+        displayManager.defaultSession = "none+herbstluftwm";
+        windowManager.herbstluftwm = enabled;
       };
       dbus = enabled;
       getty.autologinUser = config.shangrila.user.name;
     };
 
     shangrila.home.extraOptions = hm: {
-      home.file.".config/bspwm" = {
+      home.file.".config/herbstluftwm" = {
         source =
-          hm.config.lib.file.mkOutOfStoreSymlink "/home/${config.shangrila.user.name}/.nix/modules/apps/gui/bspwm/config";
+          hm.config.lib.file.mkOutOfStoreSymlink "/home/${config.shangrila.user.name}/.nix/modules/apps/gui/herbstluftwm/config";
         recursive = true;
       };
       home.file."Pictures/wallpaper.jpg" = {
         source =
-          hm.config.lib.file.mkOutOfStoreSymlink "/home/${config.shangrila.user.name}/.nix/modules/apps/gui/bspwm/wallpaper.jpg";
+          hm.config.lib.file.mkOutOfStoreSymlink "/home/${config.shangrila.user.name}/.nix/modules/apps/gui/herbstluftwm/wallpaper.jpg";
       };
       #TODO: See if the use of xsession could be a better way of doing it
       home.file.".xinitrc" = {
         text = ''
         ${cfg.XtraConfig}
-        exec dbus-launch --exit-with-x11 bspwm
+        exec dbus-launch --exit-with-x11 herbstluftwm
         '';
       };
     };
-
   };
 }
