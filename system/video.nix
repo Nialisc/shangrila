6620395d58d54322814150d0f4ad62b9ab304778
Sylvain Colignon <sylvain.colignon@wttj.co>
2024-05-22 14:27:58 +0200

    chore: nvidia

--- system/video.nix
+++ system/video.nix
@@ -1,14 +1,16 @@
-{ pkgs, env, lib, ... }:
+{ pkgs, env, lib, config,  ... }:
 
 {
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware = {
-    nvidia = {
-      modesetting.enable = true;
-      powerManagement.enable = false;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    nvidia = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "555.42.02";
+      sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+      sha256_aarch64 = lib.fakeSha256;
+      openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+      settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+      persistencedSha256 = lib.fakeSha256;
     };
 
     opengl = {
